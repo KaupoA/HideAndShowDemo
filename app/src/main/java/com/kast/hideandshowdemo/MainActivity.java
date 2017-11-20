8d9829be58792ea8036a2c88842780c785be4d84
Kaupo Aun <kaupo.aun@gmail.com>
2017-11-20 19:24:41 +0200

    Demo done

--- app/src/main/java/com/kast/hideandshowdemo/MainActivity.java
+++ app/src/main/java/com/kast/hideandshowdemo/MainActivity.java
@@ -2,12 +2,31 @@ package com.kast.hideandshowdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView showHideText;
+
+    public void showClick (View view) {
+
+        showHideText.setVisibility(View.VISIBLE);
+
+    }
+
+    public void hideClick (View view) {
+
+        showHideText.setVisibility(View.INVISIBLE);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showHideText = (TextView)findViewById(R.id.showHideText);
+
     }
 }
